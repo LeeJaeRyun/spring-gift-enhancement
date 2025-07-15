b0c66b4da3f24396982786e0470cd4225dc47ccb
LeeJaeRyun <pinixs3@naver.com>
2025-07-15 16:24:41 +0900

    refactor: WishlistService 오타 수정

--- src/main/java/gift/wishlist/service/WishlistService.java
+++ src/main/java/gift/wishlist/service/WishlistService.java
@@ -44,7 +44,7 @@ public class WishlistService {
         Item item = itemRepository.findById(request.itemId())
                 .orElseThrow(() -> new CustomException(ErrorCode.ITEM_NOT_FOUND));
 
-        if (wishlistRepository.existsByMemberAndItem(member, item)) {
+        if (wishlistRepository.existByMemberAndItem(member, item)) {
             throw new CustomException(ErrorCode.WISH_ALREADY_EXISTS);
         }
 
@@ -58,4 +58,5 @@ public class WishlistService {
 
         wishlistRepository.deleteByMemberAndItem(member, item);
     }
+
 }
