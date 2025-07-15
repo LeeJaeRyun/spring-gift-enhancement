48d4851c097c1c7322fe0589246d4aaeb7d89443
LeeJaeRyun <pinixs3@naver.com>
2025-07-15 15:28:45 +0900

    fix: Member 엔티티에 빠진 getId() 추가

--- src/main/java/gift/member/entity/Member.java
+++ src/main/java/gift/member/entity/Member.java
@@ -25,6 +25,10 @@ public class Member {
         this.password = password;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getEmail() {
         return email;
     }
