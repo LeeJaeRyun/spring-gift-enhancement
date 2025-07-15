3257b7475c062a729b90c2db779a3ecdc0a79b7b
LeeJaeRyun <pinixs3@naver.com>
2025-07-15 15:15:00 +0900

    refactor: Member 엔티티 & MemberRepository에 JPA 적용

--- src/main/java/gift/member/entity/Member.java
+++ src/main/java/gift/member/entity/Member.java
@@ -1,18 +1,28 @@
 package gift.member.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "members")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
+    protected Member() {
+
     }
 
-    public Long getId() {
-        return id;
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 
     public String getEmail() {
