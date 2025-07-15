1bf2d2d0b8db1205eb444a1b3e238598d376ef0d
LeeJaeRyun <pinixs3@naver.com>
2025-07-15 15:33:16 +0900

    refactor: Item 엔티티를 JPA 기반 구조로 변경

--- src/main/java/gift/item/entity/Item.java
+++ src/main/java/gift/item/entity/Item.java
@@ -1,13 +1,29 @@
 package gift.item.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 20)
     private String name;
+
+    @Column(nullable = false)
     private Integer price;
+
+    @Column(nullable = false)
     private String imageUrl;
 
-    public Item(Long id, String name, Integer price, String imageUrl) {
-        this.id = id;
+    protected Item() {
+
+    }
+
+    public Item(String name, Integer price, String imageUrl) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
