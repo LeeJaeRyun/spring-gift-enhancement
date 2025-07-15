45ecab0971434fc0ef345fda4e14cd0904199956
LeeJaeRyun <pinixs3@naver.com>
2025-07-15 16:20:40 +0900

    refactor: Wishlist 엔티티 JPA방식으로 변경

--- src/main/java/gift/wishlist/entity/Wishlist.java
+++ src/main/java/gift/wishlist/entity/Wishlist.java
@@ -2,21 +2,31 @@ package gift.wishlist.entity;
 
 import gift.item.entity.Item;
 import gift.member.entity.Member;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "wishlist")
 public class Wishlist {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
     private Item item;
 
-    public Wishlist(Long id, Member member, Item item) {
-        this.id = id;
-        this.member = member;
-        this.item = item;
+    protected Wishlist() {
+
     }
 
     public Wishlist(Member member, Item item) {
-        this(null, member, item);
+        this.member = member;
+        this.item = item;
     }
 
     public Long getId() {
