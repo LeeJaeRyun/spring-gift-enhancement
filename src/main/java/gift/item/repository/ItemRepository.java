0e9a9b9da29ff6d9c30787496e5db626d03d81fe
LeeJaeRyun <pinixs3@naver.com>
2025-07-15 15:50:27 +0900

    refactor: ItemRepository를 JPA기반 방식으로 변경

--- src/main/java/gift/item/repository/ItemRepository.java
+++ src/main/java/gift/item/repository/ItemRepository.java
@@ -2,20 +2,11 @@ package gift.item.repository;
 
 import gift.item.dto.UpdateItemDto;
 import gift.item.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface ItemRepository {
-
-    Item saveItem(Item item);
-
-    List<Item> findAllItems();
-
-    Item findItem(Long id);
-
-    void updateItem(Long id, UpdateItemDto dto);
-
-    void deleteItem(Long id);
-
-    Optional<Item> findById(Long id);
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    
 }
